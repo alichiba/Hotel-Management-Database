debe0ddbbb6316c85454ccffac4b409dd57837cd
UBC Student <alisonh2@students.cs.ubc.ca>
2023-07-31 17:17:28 -0700

    customer and management base documents

---
+++ customer.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Hotel Customer</title>
+    </head>
+
+    <body>
+        <h1>Customer</h1>
+        
+        <?php
+
+        ?>
+	</body>
+</html>
---
+++ management.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hotel Management</title>
+    </head>
+
+    <body>
+        <h1>Management</h1>
+        
+        <?php
+
+        ?>
+	</body>
+</html>
+
