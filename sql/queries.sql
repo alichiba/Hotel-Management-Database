9c11bca40d5ade32c6f8d56045560758efb68057
AviSharma01 <avisharma1204@gmail.com>
2023-08-01 15:34:52 -0700

    Added join and aggregation with GROUP BY queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -4,3 +4,17 @@ SELECT roomNum, floor, status, hotelName, hotelAddress
     FROM Room
     WHERE status = 'AVAILABLE';
 
+-- JOIN QUERY
+-- "Select the room number, floor, status, staff ID, full name of the staff associated to the room based on the input
+--  provided for the status"
+SELECT  R.roomNum, R.floor, R.status, CS.staffID, CS.firstName, CS.lastName
+    FROM  Room R, CleaningStaff_assignedBy CS
+    WHERE R.staffID = CS.staffID
+        AND R.status = :status;
+
+-- Aggregation with GROUP BY
+-- "Find the number of customers for each hotel"
+SELECT R.hotelName, COUNT(*) AS numOfCustomers
+    FROM Reservation R
+    GROUP BY R.hotelName
+    ORDER BY numOfCustomers ASC;
