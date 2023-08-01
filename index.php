f8aaadb5a3475b7e5283210b4864b7da0bcd5314
UBC Student <alisonh2@students.cs.ubc.ca>
2023-07-31 17:09:00 -0700

    test rewrite file

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-test test
+<html>
+    <head>
+        <title>Hotel Booking and Management</title>
+    </head>
+
+    <body>
+        <h1>Hotel Booking and Management System</h1>
+        
+        <h2>Choose your view:</h2>
+        <a href="management.php"><button type="button">Management Employee</button></a>
+        <a href="customer.php"><button type="button">Customer</button></a>
+	</body>
+</html>
