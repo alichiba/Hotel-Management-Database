8030a2e0e26af2ec83c01e429ecc32ee6c846e4c
UBC Student <alisonh2@students.cs.ubc.ca>
2023-08-08 14:06:34 -0700

    Create drop.sql

---
+++ sql/drop.sql
@@ -0,0 +1,22 @@
+-- select 'drop table '||table_name||' cascade constraints;' from user_tables;
+
+-- 'DROPTABLE'||TABLE_NAME||'CASCADECONSTRAINTS;'
+-- --------------------------------------------------------------------------------
+drop table ASSIGNMENT cascade constraints;
+drop table BILLING_HAS cascade constraints;
+drop table CITYSEASON cascade constraints;
+drop table CLEANINGSTAFF_ASSIGNEDBY cascade constraints;
+drop table CUSTOMER cascade constraints;
+drop table DELUXE cascade constraints;
+drop table EMAILPASSWORD cascade constraints;
+drop table EXECUTIVE cascade constraints;
+drop table FAMILY cascade constraints;
+drop table HOTELCUSTOMER cascade constraints;
+drop table HOTELLOCATION cascade constraints;
+drop table MANAGEMENTEMPLOYEE_MANAGES cascade constraints;
+drop table RESERVATION cascade constraints;
+drop table ROOM cascade constraints;
+drop table ROOMTYPE cascade constraints;
+drop table STANDARD cascade constraints;
+drop table SUITE cascade constraints;
+drop table VIEWS cascade constraints;
